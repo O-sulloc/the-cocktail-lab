05516e43b77bb5c636881413b5a088620288ae83
O-sulloc <bujjaf@gmail.com>
2025-05-07 11:20:55 +0100

    feat: implement global layout with header and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Banner from "@/components/layout/Banner";
-import Navbar from "@/components/layout/Navbar";
 import Hero from '@/components/Hero';
 import Awards from "@/components/Awards";
 import Products from '@/components/Products';
@@ -11,13 +9,10 @@ import Reviews from "@/components/Reviews";
 import Bars from "@/components/Bars";
 import Brands from '@/components/Brands';
 import FAQ from '@/components/FAQ';
-import Footer from "@/components/layout/Footer";
 
 export default function Home() {
   return (
     <main>
-      <Banner />
-      <Navbar />
       <Hero />
       <Awards />
       <Products />
@@ -29,7 +24,6 @@ export default function Home() {
       <Bars />
       <Brands />
       <FAQ />
-      <Footer />
     </main>
   );
 }
